3750adaecd611dd82de63154e77a809f1461ed20
sagar-shetty21 <5agarm5hetty05@gmail.com>
2023-09-01 20:06:15 +0530

    creating  post done

--- admin/src/components/post/AllPosts.jsx
+++ admin/src/components/post/AllPosts.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import './styles.css';
 
 const AllPosts = () => {
+
+  const [postsData, setPostsData] = useState()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${process.env.REACT_APP_API_ENDPOINT}/posts/all_posts`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+        const data = await response.json();
+        setPostsData(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+
   return (
-    <div>AllPosts</div>
+    <div className="posts-container">
+      <div className="post-card">
+        hey
+      </div>
+    </div>
   )
 }
 
--- admin/src/components/post/NewPost.jsx
+++ admin/src/components/post/NewPost.jsx
@@ -1,8 +1,100 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './styles.css'
 
 const NewPost = () => {
+
+  const [textData, setTextData] = useState("");
+  const [imageData, setImageData] = useState([]);
+
+  const AddImage = (e) => {
+    const newFile = e.target.files[0];
+    const fileSize = newFile.size;
+    if (fileSize <= 1048576) {
+      setImageData(prev => [...prev, newFile]);
+    }else{
+      alert("Please upload Images of size below 1MB!")
+    }
+  }
+
+  const removeImage = (indexToRemove) => {
+    setImageData(prev => prev.filter((_, index) => index !== indexToRemove));
+  };
+
+  const LoadImgPreview = () => (
+    <div className="selected-img-preview">
+      {imageData.map((file, index) => (
+        <div key={index} className="preview-item">
+          <img src={URL.createObjectURL(file)} alt={`Preview ${index}`} />
+          <button onClick={() => removeImage(index)}>Remove</button>
+        </div>
+      ))}
+    </div>
+  )
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    
+    const base64DataArray = await Promise.all(
+      imageData.map(async (file) => {
+        const base64Data = await ConvertBase64(file);
+        return base64Data;
+      })
+    );
+
+    const PayloadData = {text: textData, images: {base64DataArray}}
+
+    fetch(`${process.env.REACT_APP_API_ENDPOINT}/posts/create`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(PayloadData)
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data.error) {
+        console.log(data.error.code)
+        alert("Server Error!")
+      }else{
+        alert("submitted successfully")
+      }
+    })
+  }
+
+  const ConvertBase64 = (ImgArray) => {
+    return new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(ImgArray);
+
+      fileReader.onload = () => {
+        resolve(fileReader.result)
+      }
+
+      fileReader.onerror = () => {
+        reject(fileReader.error)
+      }
+    })
+  }
+
   return (
-    <div>NewPost</div>
+    <div className="new-post-page">
+      <div className="new-post-container">
+        <form onSubmit={handleSubmit}>
+        <div className="page-title">Create a New Post</div>
+        <div className="new-post-content">  
+          <textarea value={textData} onChange={(e) => setTextData(e.target.value)} required placeholder='caption or notification goes here....'/>  
+          <LoadImgPreview />
+        </div>
+        <div className="new-post-btns">
+          <div className="img-upload-btn-wrapper">
+            <input type="file" onChange={AddImage} />
+            <button>Upload an Image</button>
+          </div>
+        </div>
+        <button type="submit" className="submit-post-btn">Publish</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
